7cf38ad9d4465990ebfb9f14a49665f7c2d59fa3
Tobin Cooney <tobin.cooney@gmail.com>
2021-12-13 13:57:54 -0500

    host form.html through test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var files = fs.readdirSync('.');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(String(files));
-}).listen(8080);
+  res.writeHead(200, { 'content-type': 'text/html' });
+  fs.createReadStream('form.html').pipe(res);
+}).listen(8000);
 
