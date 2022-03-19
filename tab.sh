7fc8a075acd5246f43d4005f7df39b3eb6af5bbd
Tobin Cooney <tobin.cooney@gmail.com>
2022-03-19 19:31:59 -0400

    a little update script

---
+++ tab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# a little script to set everything up after updating stuff
+
+echo "copying index.php to subdirectories"
+
+# update this list as the folder structure on the remote changes
+cp index.php archive/2021/iac/
+cp index.php archive/2021/darkroom/
+cp index.php archive/2021/adv-port-2/
+cp index.php archive/2021/adv-port-2/merge-conflict/
+cp index.php archive/2022/adv-pho-2/
+cp index.php archive/2022/painting/
